960145e3f23dc66ff655ca9cabbb5450b604ee01
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-05-11 17:07:06 -0400

    Change the web style

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,12 +2,12 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		colors: { 
-			'primary': '#1e1e1e',
-			'accent-color': '#d8491d',
-			'secondary-accent': '#03598a',
-			'font-accent': '#e4e4e4',
-		},
+		// colors: { 
+		// 	'primary': '#1e1e1e',
+		// 	'accent-color': '#d8491d',
+		// 	'secondary-accent': '#03598a',
+		// 	'font-accent': '#e4e4e4',
+		// },
 		extend: {},
 	},
 	plugins: [],
