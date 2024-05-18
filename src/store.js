66e10618397d85e3940be63a3fc3b4c3bff1b0c5
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-05-18 16:41:42 -0400

    Elimina lineas inutilizadas

--- src/store.js
+++ src/store.js
@@ -3,8 +3,5 @@ import { atom } from 'nanostores'
 //Estado para controlar el navbar de dispositivos de pantalla pequeña
 export const showNavMenu = atom(false)
 
-//Estado para controlar si el formulario de contacto está abierto o no
-export const isContctUsFormOpen = atom(false)
-
 //Estado para controlar que un elemento de la tarjeta de servicios sea preseleccionado al momento de presionar el botón de cotizar
 export const selectedService = atom("")
