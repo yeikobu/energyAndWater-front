e01082b20a1b975863d35fb70fd378b618a55fe6
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2024-05-19 14:27:48 -0400

    Agregando comentarios y eliminando código que no estaba siendo usado

--- src/store.js
+++ src/store.js
@@ -2,6 +2,3 @@ import { atom } from 'nanostores'
 
 //Estado para controlar el navbar de dispositivos de pantalla pequeña
 export const showNavMenu = atom(false)
-
-//Estado para controlar que un elemento de la tarjeta de servicios sea preseleccionado al momento de presionar el botón de cotizar
-export const selectedService = atom("")
